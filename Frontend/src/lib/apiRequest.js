512149e0da539107f8f26a18545e8110b7c084c8
Sakshi Maulekhi <sakshimaulekhi84@gmail.com>
2026-04-17 10:22:25 +0530

    for deployment

--- Frontend/src/lib/apiRequest.js
+++ Frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://real-estate-zx4p.onrender.com",
   withCredentials: true,
 });
 
