758d88d3307986916957ffad6742b020167a8f21
Sakshi Maulekhi <sakshimaulekhi84@gmail.com>
2026-04-17 10:06:01 +0530

    commit changes

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,9 @@ import messageRoute from "./routes/message.route.js";
 const app = express();
 console.log("CLIENT_URL:", process.env.CLIENT_URL);
 app.use(cors({
-  origin: "https://real-estate-neon-psi.vercel.app/",
+  origin: ["https://real-estate-neon-psi.vercel.app/",
+    CLIENT_URL
+  ],
   credentials: true
 }));
 app.use(express.json());
